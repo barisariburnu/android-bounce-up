fe80f7d1d9db644b9226c726099bb89a283c5c93
Barış Arıburnu <barisariburnu@gmail.com>
2022-06-25 22:58:42 +0300

    Add all files

---
+++ docs/js/config.js
@@ -0,0 +1,25 @@
+var companyDomain = 'com';
+var companyName = 'actionstudios';
+var gameName = 'bounceup';
+
+configProperties = {
+	itemType: 'admob', //  admob or facebook
+	//itemType: 'admob',
+	itemTitleNameFb: 'Smash Ball: (Facebook Ads + Android Studio + ready to publish )',
+	itemTitleNameAdmob: 'Bounce Up : (Admob + reward video + Android Studio + ready to publish )',
+	createDate:'12-Jan-2022',
+	updateDate:'12-Jan-2022',
+	authorName:'Breakbounce',
+	marketPlace:'https://appdave.com',
+	contactLabel:'Codecanyon profile',
+	contactMe:'https://codecanyon.net/user/breakbounce',
+	authorMail:'naveenkg.phd@gmail.com',
+	authorSkype:'live:.cid.e7c2f3e90e75ede8',
+	gameHashtag:'#zigzagzig',
+	companyDomain : companyDomain,
+	companyName : companyName,
+	gameName : gameName,
+	gamePackage:companyDomain+"."+companyName+"."+gameName,
+	sdkVersion: '30',
+	buildTools: '30.0.1'
+}
